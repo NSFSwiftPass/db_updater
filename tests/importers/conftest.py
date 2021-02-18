c158f03e5e0f8885841c142d3c97b830913b8d6c
nikoaliro <nick@aliroquantum.com>
2021-02-17 19:46:32 -0700

    fixture param

--- tests/importers/conftest.py
+++ tests/importers/conftest.py
@@ -1,14 +1,14 @@
 from datetime import timedelta
 from shutil import rmtree
 
-from pytest import fixture
+from pytest import fixture, mark
 
 from db_updater.importers.uls_data_retriever import UlsDataRetriever
 from db_updater.utils import get_today_uls
 
 
-@fixture
-def retriever():
+@fixture(params=[True, False])
+def retriever(request):
     new_test_directory = 'new_test_directory'
     today = get_today_uls()
     yesterday = today - timedelta(days=1)
@@ -17,7 +17,7 @@ def retriever():
         yield UlsDataRetriever(datetime_from=yesterday,
                                datetime_to=today,
                                directory_path=new_test_directory,
-                               is_daily=True)
+                               is_daily=request.param)
     finally:
         try:
             rmtree(new_test_directory)
